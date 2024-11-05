e778e7e47ccfeeb6c971afe55ecc69224813a2b9
Allison <allisonabinger@gmail.com>
2024-11-05 13:13:22 -0600

    A - 11/05 01:13 PM - Applied accurate styling to search bars for title and release year

--- components/SearchFilter.tsx
+++ components/SearchFilter.tsx
@@ -23,23 +23,23 @@ export default function SearchFilter({ onSearch }: SearchFilterProps) {
 
   return (
     <div className="flex flex-col p-0 mb-4">
-      <div className="flex flex-col">
+      <div className="flex flex-col mb-4">
         <h2>Search</h2>
         <input
           type="text"
           placeholder="Search Movies..."
           onChange={(e) => setQuery(e.target.value)}
-          className="input-box"
+          className="border-2 border-teal rounded-full p-1 bg-navy"
         />
       </div>
-      <div className="flex flex-row">
+      <div className="flex flex-row space-x-2">
         <div className="flex flex-col">
             <h2>Min Year</h2>
-            <input type="text" placeholder="1990" value={minYear || ""} className="input-box" onChange={(e) => setMinYear(Number(e.target.value))}/>
+            <input type="text" placeholder="1990" value={minYear || ""} className="border-2 border-teal rounded-full p-1 bg-navy" onChange={(e) => setMinYear(Number(e.target.value))}/>
         </div>
         <div className="flex flex-col">
             <h2>Max Year</h2>
-            <input type="text" placeholder="2024" value={maxYear || ""} className="input-box" onChange={(e) => setMaxYear(Number(e.target.value))}/>
+            <input type="text" placeholder="2024" value={maxYear || ""} className="border-2 border-teal rounded-full p-1 bg-navy" onChange={(e) => setMaxYear(Number(e.target.value))}/>
         </div>
       </div>
     </div>
