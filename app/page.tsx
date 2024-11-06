54574786cd0ee0781b6d66621e5bf0f5a6526cbf
Allison <allisonabinger@gmail.com>
2024-11-06 11:47:15 -0600

    A - 11/06 11:47 AM - Added loading animation

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import MovieList from "@/components/MovieList";
 import PageButtons from "@/components/PageButtons";
 import SearchFilter from "@/components/SearchFilter";
 import { useEffect, useState } from "react";
+import { Player } from "@lottiefiles/react-lottie-player";
+import Loading from "@/assets/lotties/loading.json"
 
 // homepage for non-logged-in users
 
@@ -21,40 +23,44 @@ export default function Page() {
   const [minYear, setMinYear] = useState(1900);
   const [maxYear, setMaxYear] = useState(2024);
   const [genres, setGenres] = useState<string[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [filteredMovies, setFilteredMovies] = useState<Movie[]>([])
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [filteredMovies, setFilteredMovies] = useState<Movie[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [totalPages, setTotalPages] = useState<number>(1);
+  const moviesPerPage = 9;
 
   // fetch funciton
   async function fetchMovies() {
-    if (isLoading) return;
     setIsLoading(true);
     try {
-        const params = new URLSearchParams({
-          page: "1",
-          minYear: minYear.toString(),
-          maxYear: maxYear.toString(),
-        });
-    
-        if (query) {
-          params.append("query", query);
-        }
-    
-        if (genres.length > 0) {
-          params.append("genres", genres.join(","));
-        }
-    
-        const fetchURL = `/api/titles?${params.toString()}`;
-        console.log(fetchURL);
-    
-        const response = await fetch(`/api/titles?${params.toString()}`);
-        const data = await response.json();
-    
-        console.log(data);
-        setAllMovies(data.title);
-    
-        setIsLoading(false);
+      const params = new URLSearchParams({
+        moviesPerPage: moviesPerPage.toString(),
+        minYear: minYear.toString(),
+        maxYear: maxYear.toString(),
+      });
+
+      if (query) {
+        params.append("query", query);
+      }
+
+      if (genres.length > 0) {
+        params.append("genres", genres.join(","));
+      }
+
+      const fetchURL = `/api/titles?${params.toString()}`;
+      console.log(fetchURL);
+
+      const response = await fetch(fetchURL);
+      const data = await response.json();
+
+      console.log(data);
+      setAllMovies(data.title);
+
+      setIsLoading(false);
     } catch (err) {
-        console.error('Error fetching titles: ', err);
+      console.error("Error fetching titles: ", err);
+    } finally {
+        setIsLoading(false)
     }
   }
 
@@ -65,38 +71,56 @@ export default function Page() {
 
   useEffect(() => {
     const filtered = allMovies.filter((movie) => {
-        // search query
-        const matchesQuery = movie.title.toLowerCase().includes(query.toLowerCase())
-        // genre
-        const matchesGenre = genres.length === 0 || genres.includes(movie.genre);
-        // min/max year range
-        const matchesYears = movie.released >= minYear && movie.released <= maxYear;
+      // search query
+      const matchesQuery = movie.title
+        .toLowerCase()
+        .includes(query.toLowerCase());
+      // genre
+      const matchesGenre = genres.length === 0 || genres.includes(movie.genre);
+      // min/max year range
+      const matchesYears =
+        movie.released >= minYear && movie.released <= maxYear;
 
-        return matchesGenre && matchesGenre && matchesYears;
+      return matchesGenre && matchesGenre && matchesYears;
     });
-    setFilteredMovies(filtered)
-  }, [allMovies, query, genres, minYear, maxYear])
+    setFilteredMovies(filtered);
+  }, [allMovies, query, genres, minYear, maxYear]);
 
   return (
-    <main className="flex flex-col mb-0">
-      <div className="search-filter w-full p-4 pb-0 mb-0 flex justify-between text-white">
-        {/* Search Forms */}
-        <SearchFilter
-          onSearch={(query, minYear, maxYear) => {
-            setQuery(query);
-            setMinYear(minYear);
-            setMaxYear(maxYear);
-          }}
-        />
-        <GenreFilter
-          onGenreSelect={(selectedGenres) => setGenres(selectedGenres)}
-        />
-      </div>
-      <div className="w-auto p-5 mx-0 flex justify-center">
-        <div className="w-full flex justify-center">
-          <MovieList movieList={filteredMovies} />
+    <div className="flex flex-col mb-0">
+      {isLoading ? (
+        <div className="flex justify-center items-center h-screen w-full">
+          <Player
+            src={Loading}
+            className="loading"
+            loop
+            autoplay
+            speed={3}
+            style={{ height: "500px", width: "500px" }}
+          />
         </div>
-      </div>
-    </main>
+      ) : (
+        <>
+          <div className="search-filter w-full p-4 pb-0 mb-0 flex justify-between text-white">
+            {/* Search Forms */}
+            <SearchFilter
+              onSearch={(query, minYear, maxYear) => {
+                setQuery(query);
+                setMinYear(minYear);
+                setMaxYear(maxYear);
+              }}
+            />
+            <GenreFilter
+              onGenreSelect={(selectedGenres) => setGenres(selectedGenres)}
+            />
+          </div>
+          <div className="w-auto p-5 mx-0 flex justify-center">
+            <div className="w-full flex-col justify-center">
+              <MovieList movieList={filteredMovies} />
+            </div>
+          </div>
+        </>
+      )}
+    </div>
   );
 }
