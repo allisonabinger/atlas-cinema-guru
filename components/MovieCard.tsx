bb1d9c4d452891edf37afe7d4efea42c61b7516a
Allison <allisonabinger@gmail.com>
2024-11-07 17:46:06 -0600

    A - 11/07 05:45 PM - added watch-later and favorites funcitonality, tested with API calls and pulls data to determine status AND completes post/delete functions. movie list calls GET and passes as bool.

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,49 +1,81 @@
+"use client";
 import Link from "next/link";
 import StarOutline from "@/assets/icons/staroutline.svg";
 import StarFull from "@/assets/icons/starfull.svg";
 import ClockOutline from "@/assets/icons/clockoutline.svg";
 import ClockFull from "@/assets/icons/clockfull.svg";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 interface Movie {
-    id: string;
-    title: string;
-    synopsis: string;
-    released: number;
-    genre: string;
-    favorited?: boolean;
-    watchLater?: boolean;
-    image: string;
+  id: string;
+  title: string;
+  synopsis: string;
+  released: number;
+  genre: string;
+  favorited: boolean;
+  watchLater: boolean;
+  image: string;
 }
 
 type MovieProps = {
-    movie: Movie;
+  movie: Movie;
 };
 
 export function MovieCard({ movie }: MovieProps) {
+  const [inFavorites, setInFavorites] = useState<boolean>(movie.favorited);
+  const [inWatchLater, setInWatchLater] = useState<boolean>(movie.watchLater);
+
+  const toggleFavorite = async (id: string) => {
+    console.log("favorite button pressed");
+    setInFavorites((prev) => !prev);
+    // makes api call, posts if not in faves, deletes if it is
+    const response = await fetch(`/api/favorites/${id}`, {
+      method: inFavorites ? "DELETE" : "POST",
+    });
+  };
+
+  const toggleWatchLater = async (id: string) => {
+    setInWatchLater((prev) => !prev);
+    // makes api call, posts if not in watchlater, deletes if it is
+    const response = await fetch(`/api/watch-later/${id}`, {
+      method: inWatchLater ? "DELETE" : "POST",
+    });
+  };
+
+  useEffect(() => {
+    setInFavorites(movie.favorited);
+    setInWatchLater(movie.watchLater);
+  }, [movie.favorited, movie.watchLater]);
+
+
   return (
-    <div className="relative group shadow-lg w-fit border-2 border-teal rounded-2xl flex justify-center">
-      <img src={movie.image} alt={movie.title} className="size-96 rounded-2xl" />
+    <div className="relative group shadow-lg border-2 border-teal rounded-2xl h-full w-full">
+      <img
+        src={movie.image}
+        alt={movie.title}
+        className="size-96 rounded-2xl object-cover w-full aspect-square"
+      />
 
       {/* Overlay */}
-      <div className="absolute h-fit top-3 right-3 flex space-x-2 opacity-0 group-hover:opacity-100 transition-opacity duration-200 ease-in-out">
-        <button>
-            <Image
-              src={StarFull.src}
-              alt="Favorites Icon"
-              width={30}
-              height={30}
-              className="object-cover transition-transform duration-200 transform group-hover:scale-105"
-            />
+      <div className="absolute h-fit top-3 right-3 flex opacity-0 group-hover:opacity-100 transition-opacity duration-200 ease-in-out">
+        <button onClick={() => toggleFavorite(movie.id)} className="px-3">
+          <Image
+            src={inFavorites ? StarFull.src : StarOutline.src}
+            alt="Favorites Icon"
+            width={30}
+            height={30}
+            className="object-cover transition-transform duration-200 transform group-hover:scale-105"
+          />
         </button>
-        <button>
-            <Image
-              src={ClockFull.src}
-              alt="Watch Later Icon"
-              width={30}
-              height={30}
-              className="object-cover transition-transform duration-200 transform group-hover:scale-105"
-            />
+        <button onClick={() => toggleWatchLater(movie.id)}>
+          <Image
+            src={inWatchLater ? ClockFull.src : ClockOutline.src}
+            alt="Watch Later Icon"
+            width={30}
+            height={30}
+            className="object-cover transition-transform duration-200 transform group-hover:scale-105"
+          />
         </button>
       </div>
       <div className="rounded-b-2xl absolute bottom-0 left-0 right-0 h-1/2 bg-navy opacity-0 group-hover:opacity-90 transition-opacity duration-200 flex flex-col p-4 text-left">
@@ -51,7 +83,9 @@ export function MovieCard({ movie }: MovieProps) {
           {movie.title} ({movie.released})
         </h1>
         <p className="mb-6 text-sm">{movie.synopsis}</p>
-        <div className="genre rounded-full bg-teal p-2 w-fit gap-2 text-black">{movie.genre}</div>
+        <div className="genre rounded-full bg-teal p-2 w-fit gap-2 text-black">
+          {movie.genre}
+        </div>
       </div>
     </div>
   );
--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -1,34 +1,70 @@
 // Movie grid for movie cards
-
+"use client";
+import { useEffect, useState } from "react";
 import { MovieCard } from "./MovieCard";
 
 interface Movie {
-    id: string;
-    title: string;
-    synopsis: string;
-    released: number;
-    genre: string;
-    favorited?: boolean;
-    watchLater?: boolean;
-    image: string;
+  id: string;
+  title: string;
+  synopsis: string;
+  released: number;
+  genre: string;
+  image: string;
 }
 
 interface MovieListProps {
-    movieList: Movie[];
+  movieList: Movie[];
 }
 
 export default function MovieList({ movieList }: MovieListProps) {
+  // storing favorites and watchLaters in a set for optimization when mapping titles
+  const [favorites, setFavorites] = useState<Set<string>>(new Set());
+  const [watchLater, setWatchLater] = useState<Set<string>>(new Set());
+
+  useEffect(() => {
+    const fetchFlags = async () => {
+      try {
+        const [favoritesRes, watchLaterRes] = await Promise.all([
+          fetch("/api/favorites"),
+          fetch("/api/watch-later"),
+        ]);
+        const favoritesData = await favoritesRes.json();
+        const watchLaterData = await watchLaterRes.json();
+
+        console.log("Favorites Data: ", favoritesData)
+
+        const favorites = Array.isArray(favoritesData.favorites) ? favoritesData.favorites : [];
+        const watchLater = Array.isArray(watchLaterData.favorites) ? watchLaterData.watchLater : [];
+
+        setFavorites(new Set(favorites.map((movie: Movie) => movie.id)));
+        setWatchLater(new Set(watchLater.map((movie: Movie) => movie.id)));
+
+        console.log("Favorites after stored in set: ", favorites)
+      } catch (err) {
+        console.error("Error fetching favorites and watch later data:", err);
+      }
+    };
+    fetchFlags();
+  }, []);
+
   return (
     <div className="px-24 flex justify-center">
-        <div className="grid grid-cols-3 gap-x-32 gap-y-4 mb-3">
-            {movieList ? (
-                movieList.map((movie) => (
-                    <MovieCard key={movie.id} movie={movie} />
-                ))
-            ) : (
-                <h2 className="text-white text-2xl">No titles found.</h2>
-            )}
-        </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 md:gap-x-16 lg:gap-x-8 gap-y-4 mb-3">
+        {movieList ? (
+          movieList.map((movie) => (
+            <MovieCard
+              key={movie.id}
+              movie={{
+                ...movie,
+                favorited: favorites.has(movie.id),
+                watchLater: watchLater.has(movie.id),
+              }}
+            />
+          ))
+        ) : (
+          <h2 className="text-white text-2xl">No titles found.</h2>
+        )}
+      </div>
     </div>
   );
 }
