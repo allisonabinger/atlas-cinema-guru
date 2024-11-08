adec7e056b8cd00b1c571be4ce2ceb31562106b6
Allison <allisonabinger@gmail.com>
2024-11-07 18:55:01 -0600

    A - 11/07 06:54 PM - adjusted layout for activities and Sidebar fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,11 +14,11 @@ type Props = {
 
 export default function RootLayout({ children }: Props) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-screen">
       <body className={`antialiased bg-[#00003c] text-white`}>
           <div className="flex h-screen flex-col font-poppins ">
           <NavBar />
-            <div className="top-0 left-0 bottom-0 z-40 w-full h-screen flex flex-row pl-0 ml-0">
+            <div className="top-0 left-0 bottom-0 z-40 w-full h-full flex flex-row pl-0 ml-0">
                 <SideNav />
                 <div className="w-full">
                     <main className="ui p-6">{children}</main>
